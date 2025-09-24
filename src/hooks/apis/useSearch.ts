7fb9fc83bc033cc70cc1c8178a125f40f4cb146f
NTDKhoa04 <nguyenthaidangkhoa04@gmail.com>
2025-09-24 14:24:39 +0700

    fix: Remove unused variable

--- src/hooks/apis/useSearch.ts
+++ src/hooks/apis/useSearch.ts
@@ -1,9 +1,7 @@
 import { SearchAction } from "@/apis/search.action";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 export const useSearch = () => {
-  const queryClient = useQueryClient();
-
   const useGetSearchResults = (query: string) => {
     return useQuery({
       queryKey: ["search", query],
